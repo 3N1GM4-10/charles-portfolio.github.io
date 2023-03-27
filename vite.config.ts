0ce0f0e65c19d4d27a5a5a5923bb1e646913328b
3N1GM4-10 <heculicharles25@gmail.com>
2023-03-27 18:19:18 +0800

    replace path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "/charles-portfolio.github.io/",
+  base: "./",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
