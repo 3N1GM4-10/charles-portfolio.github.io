b955d098716e91ba50a42e400bf100b7de03d45c
3N1GM4-10 <heculicharles25@gmail.com>
2023-03-27 17:50:04 +0800

    fixed large chunks

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   base: "/fe-ems-io/",
   plugins: [vue(), vueJsx()],
@@ -13,4 +14,15 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
 });
