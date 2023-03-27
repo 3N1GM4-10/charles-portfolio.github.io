7f6edbc9fc5a9aa4714f5b423ea82cf8e7288328
3N1GM4-10 <heculicharles25@gmail.com>
2023-03-27 18:02:36 +0800

    fixed large chunk and base in ts.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "/fe-ems-io/",
+  base: "/charles-portfolio.github.io/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
